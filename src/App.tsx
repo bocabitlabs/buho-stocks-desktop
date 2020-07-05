dee7de7f9ec71aec10085d064bd015000837f630
René Fernández <renefs87@gmail.com>
2020-07-05 21:57:29 +0200

    Add example of typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'antd';
+import {
+  ExampleComponent,
+  ExampleComponentWithType
+} from './components/ExampleComponent';
 
 function App() {
+  /**
+   * Main
+   */
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <ExampleComponent who={'me'} />
+          <ExampleComponentWithType who={'me2'} />
         </p>
         <Button type="primary">Learn React</Button>
       </header>
---
+++ src/components/ExampleComponent.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+/**
+ *
+ * @param who: Who is this
+ * @type who: string
+ */
+export const ExampleComponent = ({ who }: { who: string }) => {
+  return <div>Hello this is {who}</div>;
+};
+
+/**
+ *
+ */
+interface ExampleProps {
+  /**
+   * Who will be blabla
+   */
+  who: string;
+  /**
+   * Example of optional parameter
+   */
+  optionalParam?: string;
+}
+
+export const ExampleComponentWithType = ({
+  who,
+  optionalParam
+}: ExampleProps) => {
+  return <div>Hello this is {who}</div>;
+};
