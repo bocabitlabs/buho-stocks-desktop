b6efda0c25f1e4220c44e21fe591cab5dfa82988
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2020-08-11 20:53:58 +0200

    Add tests for CompanyListItem

---
+++ src/components/CompanyListItem/CompanyListItem.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CompanyListItem from "./CompanyListItem";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+
+describe("CompanyListItem component tests", () => {
+  const mockStore = configureStore();
+  const history = createMemoryHistory();
+
+  let store;
+  let initialState;
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("renders company item spinner", () => {
+    initialState = {
+      firebase: {
+        auth: {}
+      },
+      firestore: {
+        data: {
+          companies: {
+            abcdefg: {
+              name: "Test Company",
+              ticker: "TEST:TC"
+            }
+          }
+        },
+        ordered: {
+          companies: []
+        }
+      }
+    };
+    store = mockStore(initialState);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+        <CompanyListItem id={"abcdefg"} />
+
+        </Router>
+      </Provider>
+    );
+    const element = getByText(/Test Company/i);
+    expect(element).toBeInTheDocument();
+  });
+});
