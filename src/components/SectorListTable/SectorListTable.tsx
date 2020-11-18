32a5cae33ba745b2a61d472ef43b8d2d1813c190
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2020-11-18 20:47:35 +0100

    Add delete button to sectors list

--- src/components/SectorListTable/SectorListTable.tsx
+++ src/components/SectorListTable/SectorListTable.tsx
@@ -1,24 +1,47 @@
-import { Space, Table } from "antd";
+import { Button, Popconfirm, Space, Table } from "antd";
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { SectorsContext } from "../../contexts/sectors";
+import SectorService from "../../services/sector-service";
 import { SectorItemProps } from "../../types/sector";
 
 export default function SectorListTable() {
   const { sectors } = useContext(SectorsContext);
+  const history = useHistory();
+
+  function confirm(recordId: string) {
+    const result = new SectorService().deleteSectorById(recordId);
+    if (result === "OK") {
+      history.push({
+        pathname: "/currencies",
+        state: {
+          message: { type: "success", text: "Currency has been deleted" }
+        }
+      });
+    }
+  }
 
   const columns = [
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      render: (text: string) => <a>{text}</a>
+      render: (text: string) => text
     },
     {
       title: "Action",
       key: "action",
-      render: (text: string, record: { name: string }) => (
+      render: (text: string, record: any) => (
         <Space size="middle">
-          <a>Delete</a>
+          <Popconfirm
+            key={`sector-delete-${record.key}`}
+            title={`Delete sector ${record.name}?`}
+            onConfirm={() => confirm(record.key)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button>Delete</Button>
+          </Popconfirm>
         </Space>
       )
     }
